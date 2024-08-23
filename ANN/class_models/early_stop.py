dc018cc1be6b6796e283dce9c3aa680ad07190e2
ombolanos <omarferbol@gmail.com>
2024-08-22 19:07:24 -0500

    updating project

--- ANN/class_models/early_stop.py
+++ ANN/class_models/early_stop.py
@@ -3,36 +3,35 @@ import copy
 
 
 class EarlyStopping:
-  def __init__(self, patience=5, min_delta=0, restore_best_weights=True):
-    self.patience = patience
-    self.min_delta = min_delta
-    self.restore_best_weights = restore_best_weights
-    self.best_model = None
-    self.best_loss = None
-    self.patience_counter = 0
-    self.status = ""
+    def __init__(self, patience=5, min_delta=0, restore_best_weights=True):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.restore_best_weights = restore_best_weights
+        self.best_model = None
+        self.best_loss = None
+        self.patience_counter = 0
+        self.status = ""
 
-  def reset(self):
-    self.status = ""
-    self.patience_counter = 0
+    def reset(self):
+        self.status = ""
+        self.patience_counter = 0
 
-  def __call__(self, model, val_loss):
-    if self.best_loss is None:
-      self.best_loss = val_loss
-      self.best_model = copy.deepcopy(model.state_dict())
-    elif self.best_loss - val_loss >= self.min_delta:
-      self.best_model = copy.deepcopy(model.state_dict())
-      self.best_loss = val_loss
-      self.status = f"Improvement!!!, actual counter {self.patience_counter}"
-      self.patience_counter = 0
-    else:
-      self.patience_counter += 1
-      self.status = f"NO improvement in the last {self.patience_counter} epochs"
-      if self.patience_counter >= self.patience:
-        self.status = f"Early stopping triggered after {self.patience_counter} epochs."
-        if self.restore_best_weights:
-          model.load_state_dict(self.best_model)
-        self.reset()
-        return True
-    return False
-
+    def __call__(self, model, val_loss):
+        if self.best_loss is None:
+            self.best_loss = val_loss
+            self.best_model = copy.deepcopy(model.state_dict())
+        elif self.best_loss - val_loss >= self.min_delta:
+            self.best_model = copy.deepcopy(model.state_dict())
+            self.best_loss = val_loss
+            self.status = f"Improvement!!!, actual counter {self.patience_counter}"
+            self.patience_counter = 0
+        else:
+            self.patience_counter += 1
+            self.status = f"NO improvement in the last {self.patience_counter} epochs"
+            if self.patience_counter >= self.patience:
+                self.status = f"Early stopping triggered after {self.patience_counter} epochs."
+                if self.restore_best_weights:
+                    model.load_state_dict(self.best_model)
+                self.reset()
+                return True
+        return False
